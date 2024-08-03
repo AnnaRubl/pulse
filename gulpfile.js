c9d913104c2fbcbd26387472768447c92ab08604
nastia <nastiaaaa3467@gmail.com>
2024-08-03 15:02:04 +0200

    mod gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -7,23 +7,19 @@ const cleanCSS = require('gulp-clean-css');
 
 // Static server
 gulp.task('server', function() {
-    browserSync.init({
+    browserSync({
         server: {
             baseDir: "src"
         }
     });
+    gulp.watch("src/*.html").on("change", browserSync.reload);
 });
 
 gulp.task('styles', function() {
     return gulp.src("src/sass/*.+(scss|sass)")
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(rename({
-            prefix: "",
-            suffix: ".min",
-            }))
-        // .pipe(autoprefixer({
-        //         cascade: false
-        //     }))
+        .pipe(rename({prefix: "",suffix: ".min",}))
+        //.pipe(autoprefixer())
         .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(gulp.dest("src/css"))
         .pipe(browserSync.stream());
@@ -31,7 +27,6 @@ gulp.task('styles', function() {
 
 gulp.task('watch', function(){
     gulp.watch("src/sass/*.+(scss|sass)", gulp.parallel("styles"));
-    gulp.watch("src/*.html").on("change", browserSync.reload);
 });
 
 gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
