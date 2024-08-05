cc154db07540ab7f40342e16f2266c46ef341ecd
nastia <nastiaaaa3467@gmail.com>
2024-08-05 20:34:25 +0200

    add second block advantages

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require('gulp');
 const browserSync = require('browser-sync');
 const sass = require('gulp-sass')(require('sass'));
 const rename = require("gulp-rename");
-// const autoprefixer = require('gulp-autoprefixer');
+const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
 
 // Static server
@@ -16,17 +16,17 @@ gulp.task('server', function() {
 });
 
 gulp.task('styles', function() {
-    return gulp.src("src/sass/*.+(scss|sass)")
+    return gulp.src("src/sass/**/*.+(scss|sass)")
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(rename({prefix: "",suffix: ".min",}))
-        //.pipe(autoprefixer())
+        .pipe(autoprefixer())
         .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(gulp.dest("src/css"))
         .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function(){
-    gulp.watch("src/sass/*.+(scss|sass)", gulp.parallel("styles"));
+    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel("styles"));
 });
 
 gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
